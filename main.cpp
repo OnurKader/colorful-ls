f7c4e5c78627c96ff03187f5c080f81e463c544a
OnurKader <onurkader@protonmail.com>
2020-06-21 20:58:35 +0300

    Initial Commit

--- main.cpp
+++
@@ -1,7 +0,0 @@
-#include <fmt/core.h>
-
-int main()
-{
-	fmt::print("OwO\n");
-	return 0;
-}
---
+++ src/List.cpp
@@ -0,0 +1,10 @@
+#include "Args.hpp"
+
+#include <fmt/core.h>
+
+int main(int argc, char** argv)
+{
+	fmt::print("Hello\n");
+
+	return 0;
+}
