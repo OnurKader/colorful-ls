9ae6f20418742cd4b95bfe1adbffbc3c2d93eb91
OnurKader <onurkader@protonmail.com>
2020-07-31 04:09:42 +0300

    Removed some comments, commented code included.

--- src/List.cpp
+++ src/List.cpp
@@ -18,39 +18,11 @@ int main(int argc, char** argv)
 		return 69;
 	const auto results = results_opt.value();
 
-	// FIXME: put positional arguments in a vector and do printings for those, if it's a directory
-	// put the name of it above the listings
-
 	// MAYBE: Do CTRE regex stuff as an argument?
 	// man glob
 	if(argc == 1)
 	{
 		OK::File input_file {"."};
-		// Unnecessary check?
-		/*
-		if(!fs::exists(input_file.path()))
-		{
-			fmt::print(
-				stderr, "    {}File or directory not found{}\n", OK::Color::RED, OK::Color::RESET);
-			return 1;
-		}
-		else if(fs::is_directory(input_file.path()))
-		{
-			const auto num_of_files_in_directory =
-				std::distance(std::filesystem::directory_iterator {input_file.path()},
-							  std::filesystem::directory_iterator {});
-
-			if(num_of_files_in_directory == 0L)
-			{
-				fmt::print(stderr, "    {}Nothing to show here...\n", OK::Color::rgb(229, 195, 38));
-				return 0;
-			}
-
-			// The actual printing
-			OK::FileVec file_vec {std::move(input_file), num_of_files_in_directory, results.all};
-
-			file_vec.print(results);
-		}*/
 		if(!fs::exists(input_file.path()))
 		{
 			fmt::print(
@@ -81,16 +53,7 @@ int main(int argc, char** argv)
 		// Multiple inputs in argv, put them in a vector and print those
 		OK::FileVec file_vec {argc, argv, results};
 		file_vec.print();
-		/*
-		else
-		{
-			fmt::print("{}",
-					   input_file.long_name_to_string(results,
-													  input_file.get_size_digit_count(),
-													  input_file.username().size(),
-													  input_file.groupname().size()));
-		}
-		*/
+
 		return file_vec.return_value();
 	}
 
